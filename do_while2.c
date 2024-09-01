02fff8caea6a6f03953ed7a794668fb7a32fefec
vishwas m v <167001715+vishwasdevmv@users.noreply.github.com>
2024-09-01 22:59:51 +0530

    Add files via upload

---
+++ do_while2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i=1;
+    printf("enter the value of n\n");
+    scanf("%d",&n);
+    do{
+        printf("%d\n",i);
+        i++;
+    }while(i<=n);
+    
+
+}
