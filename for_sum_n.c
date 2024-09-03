9779b77ef1130ea67ca7842b6657105992165ab3
vishwas m v <167001715+vishwasdevmv@users.noreply.github.com>
2024-09-03 23:36:59 +0530

    Add files via upload

---
+++ for_sum_n.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int sum = 0;
+    for (int i = 0; i <= 10; i++)
+    {
+        sum = sum + i;
+       
+    }
+     printf("the sum of natural number is %d\n", sum);
+}
