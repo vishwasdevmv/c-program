2ac5740ae3df8f59b26deb51025d70d3687ec707
vishwas m v <167001715+vishwasdevmv@users.noreply.github.com>
2024-09-06 22:23:24 +0530

    Add files via upload

---
+++ array2.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+
+int main()
+{
+	int arr[5];
+	
+	// Assume that base address of arr is 2000 and size of integer
+        // is 32 bit
+	arr++;
+	printf("%u", arr);
+	
+	return 0;
+}
